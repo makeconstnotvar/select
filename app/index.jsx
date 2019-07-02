8241de25084233eac19ea09bf688bc442959646f
Alexander <makeconstnotvar@protonmail.com>
2019-07-03 01:33:06 +0300

    fix git

--- app/index.jsx
+++ app/index.jsx
@@ -3,5 +3,5 @@ import {App} from "./App";
 import React from "react";
 import {Provider} from "mobx-react";
 import {store} from "./store";
-
+//1
 render(<Provider {...store}><App/></Provider>, document.getElementById('root'));
